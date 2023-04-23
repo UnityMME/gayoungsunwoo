adac4f9e6dca12d43a757c5aea0daa146dd08fdd
joaga0 <joaga0@naver.com>
2023-04-23 23:48:10 +0900

    Feat:almostDone

--- assignment/assignment/Assets/EnemyWalking.cs
+++ assignment/assignment/Assets/EnemyWalking.cs
@@ -1,3 +1,4 @@
+using RavingBots.CartoonExplosion;
 using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
@@ -11,190 +12,143 @@ using UnityEngine.UIElements;
 using Debug = UnityEngine.Debug;
 
 
+
 public class EnemyWalking : MonoBehaviour
 {
+    [SerializeField]
+    public ParticleSystem explosion;
+
     Vector3 position;
-    int recent;
-    int mode; //enum
-    Vector3 Target;
+    int mode; 
     Animator animator;
+    
     bool bump;
-    //Vector3 TargetUp = new Vector3(12.96f, 0.0359f, 90);
-    //Vector3 TargetDown = new Vector3(12.96f, 0.0359f, 70);
-    Vector3 velo = Vector3.zero;
-    /*ai
-    public Transform target;
-    Rigidbody rigid;
-    BoxCollider boxCollider;
-    Animator anim;
-    NavMeshAgent nav;
-    */
+    long time;
+    Stopwatch sw = new Stopwatch();
+    Stopwatch sw1 = new Stopwatch();
+    int count;
+
+
     [SerializeField] GameObject Player;
-    [SerializeField] Collider[] cols;
-    [SerializeField] LayerMask layer;
-    [SerializeField] float radius;
+
+    public static bool freeze;
+    Coroutine coroutine;
+    Vector3 FXposition;
 
 
     void Awake()
     {
         animator = GetComponent<Animator>();
-        /*ai
-        rigid = GetComponent<Rigidbody>();
-       boxCollider = GetComponent<BoxCollider>();
-       // mat = GetComponentInChildren<Material>();
-      nav= GetComponent<NavMeshAgent>();
-       // anim = GetComponent<Animator>();
-        */
-
-        bump = false;
-
+        count = 0;
+        moveSample.freeze = false;
     }
 
 
     void Start()
     {
-        Debug.Log("start");
         position = transform.position;
-        mode=1;
+        mode =1;
+        bump = false;
         transform.forward = new Vector3(0, 0, -1);
-
-
-        //Target = new Vector3(12.96f, 0.0359f, 70);
-        //position =transform.position;
-        //StartCoroutine(MoveObject());
+        
     }
 
-    // Update is called once per frame
+
     void Update()
     {
-        if (mode==1)
+        
+        if (mode == 1)
         {
             transform.forward = new Vector3(0, 0, -1);
             position.z -= 0.4f * Time.deltaTime;
             transform.position = position;
         }
-        if (mode==2)
+        if (mode == 2)
         {
             transform.forward = new Vector3(0, 0, 1);
             position.z += 0.4f * Time.deltaTime;
             transform.position = position;
 
         }
+       
         if (mode == 0)
         {
-            //position.z += 0.4f * Time.deltaTime;
             transform.position = position;
-
         }
-
-        //transform.position = Vector3.SmoothDamp(transform.position, Target, ref velo, 0.0000000000001f);
-
-        //nav.SetDestination(target.position);
-        /*
-        if (down)
-        {
-            position.z -= 1 * Time.deltaTime;
-        }
-        if (!down)
         
-            position.z += 1 * Time.deltaTime;
-        }
-        //float dir1 = Random.Range(-1f, 1f);
-        //float dir2= Random.Range(-1f, 1f);
-        destination = position;
-        transform.position = destination;
-        
-        transform.position = Vector3.MoveTowards(transform.position, destination, 1*Time.deltaTime);
-
-          */
         if (bump)
         {
-            InvokeRepeating("cartAround", 0,0.2f);
+            coroutine=StartCoroutine(Bump());
+            time = sw.ElapsedMilliseconds;
+            if (time > 1000) { StopMethod();
+                
+                bump = false;
+                animator.SetBool("isWalking", true);
+
+                mode = 2;
+                sw.Reset();
+                sw1.Start();
+            }
         }
 
-    }
-    private void cartAround()//check when cart finish bumping
-    {
-        cols = Physics.OverlapSphere(transform.position, radius,layer); //player.transform.position?
-        //if (cols.Exists(Player)) { bump = true; }
-        if (cols == null) { 
-            bump = false;
-            animator.SetBool("isWalking", true);
-            mode = recent; 
-            Debug.Log("bump Á¾·á");
-        }
+        if (sw1.ElapsedMilliseconds > 1500) { moveSample.freeze = false; sw1.Reset(); count = 0;
+                                                                                      }
+
+
+
     }
 
-    /*
-    IEnumerator MoveObject()
+   
+    IEnumerator Bump()
     {
         
-        Debug.Log("coroutine");
-        position.x+= 1 * Time.deltaTime;
-        //float dir1 = Random.Range(-1f, 1f);
-        //float dir2= Random.Range(-1f, 1f);
-        transform.position = position;
-        yield return new WaitForSeconds(2);
-        
-
-    }
-    */
-
-    private void OnTriggerEnter(Collider other)
-    {
-
-        if (other.GetComponent<Collider>().gameObject.CompareTag("Wall1"))
-        {
-            Debug.Log("walkingEnemy and wall1 bump");
-            mode = 2;
-            //transform.Rotate(Vector3.right * 1* Time.deltaTime);
-            //Target = new Vector3(12.96f, 0.0359f, 90);
-            //RotateGameObject(new Vector3(0f, (Mathf.Cos(timer) * 0.5f * 0.5f) * 180f, 0f));
-            
-
-
-        }
-        if (other.GetComponent<Collider>().gameObject.CompareTag("Player"))
+        for (float f = 1f; f >= 0; f -= 0.1f)
         {
             animator.SetBool("isWalking", false);
-            Debug.Log("BUMP cart enemy");
-            bump = true;
-            recent = mode;
             mode = 0;
-            //Target = new Vector3(12.96f, 0.0359f, 90);
-
-
+            
         }
-        /*
-        if (other.collider.gameObject.CompareTag("Wall2"))
-        //if (other.collider.tag == "Wall1")
-        {
-            transform.localEulerAngles = new Vector3(0, 0, 0);
-            down = false;
-
-        }
-       // Time.timeScale = 1;
-        
-        */
-
+        yield return null;
     }
-    
-    /*
-    private void OnTriggerExit(Collider collision)
+
+    void StopMethod()
     {
+        if (coroutine != null)
+        {
+            StopCoroutine(coroutine);
+        }
+    }
+   
+    public void OnTriggerEnter(Collider collision)
+    {
+        if (collision.GetComponent<Collider>().gameObject.CompareTag("Wall1"))
+        {
+            mode = 2;
+        }
         if (collision.GetComponent<Collider>().gameObject.CompareTag("Player"))
         {
-            Debug.Log("BUMP STOPPED");
-            mode = recent;
-            animator.SetBool("isWalking", true);
             
-            //Target = new Vector3(12.96f, 0.0359f, 90);
+            count++;
+            sw.Start();
+            FXposition = transform.position;
+            FXposition.z -= 1;
+            FXposition.y = 1f;
 
+            //transform.Rotate(new Vector3(0,1,0) * Time.deltaTime);
+
+            if (count == 1)
+            {
+            Instantiate(explosion, FXposition, Quaternion.identity);
+
+            bump = true;
+                moveSample.freeze = true;
+
+            
+            }
 
         }
-
     }
-    */
-   
+
+
 }
      
